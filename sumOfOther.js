7781744e394025a1dde579e5e71a5ab763b423b0
Hellsingi <f.c.realmadrid@mail.ru>
2018-10-28 02:23:39 +0300

    feAT: *Install eslint, eslint-config-airbnb-base and mocha*

--- sumOfOther.js
+++ sumOfOther.js
@@ -1,5 +1,5 @@
-function sumOfOther() {
+function sumOfOther(array) {
   const arr = array;
-  const sumArr = arr.reduce ((sum,current)=>sum+current,0);
+  const sumArr = arr.reduce((sum, current) => sum + current, 0);
   return arr.map(item => sumArr - item);
 }
