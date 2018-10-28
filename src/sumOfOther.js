19b012871d80d9c3ec179567a590cab8f38a17d6
Hellsingi <f.c.realmadrid@mail.ru>
2018-10-28 19:11:33 +0300

    *test:- * create a test for a function sunOfOther*

--- src/sumOfOther.js
+++ src/sumOfOther.js
@@ -1,4 +1,4 @@
-function sumOfOther(array) {
+module.exports = function sumOfOther(array) {
   const arr = array;
   const sumArr = arr.reduce((sum, current) => sum + current, 0);
   return arr.map(item => sumArr - item);
--- test/test.js
+++ test/test.js
@@ -1,8 +1,45 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
-    });
-  });
+const assert = require('assert');
+const sumOfOther = require('../src/sumOfOther');
+
+describe('sumOfOther', () => {
+	it('1', () => {
+	  assert.equal(sumOfOther([2,3,4,1]).toString(), [8,7,6,9].toString());
+	});
+
+	it('2', () => {
+	  assert.equal(sumOfOther([21,13]).toString(), [13,21].toString());
+	});
+
+	it('3', () => {
+	  assert.equal(sumOfOther([1,2,3,4,5,6,7,8,9]).toString(), [44,43,42,41,40,39,38,37,36].toString());
+	});
+
+	it('4', () => {
+	  assert.equal(sumOfOther([0,2,4,0,0,0]).toString(), [6,4,2,6,6,6].toString());
+	});
+
+	it('5', () => {
+	  assert.equal(sumOfOther([0]).toString(), [0].toString());
+	});
+
+	it('6', () => {
+	  assert.equal(sumOfOther([132,23,454,0]).toString(), [477,586,155,609].toString());
+	});
+
+	it('7', () => {
+	  assert.equal(sumOfOther([1,0,1,0,1,0]).toString(), [2,3,2,3,2,3].toString());
+	});
+
+	it('8', () => {
+	  assert.equal(sumOfOther([9,1,1]).toString(), [2,10,10].toString());
+	});
+
+	it('9', () => {
+	  assert.equal(sumOfOther([3,7,5,2,9]).toString(), [23,19,21,24,17].toString());
+	});
+
+	it('10', () => {
+	  assert.equal(sumOfOther([100000000]).toString(), [0].toString());
+	});
+
 });
