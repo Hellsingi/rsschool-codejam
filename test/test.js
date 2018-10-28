31101e3b1105f620144e6bedf15464175e6e0d8e
Hellsingi <f.c.realmadrid@mail.ru>
2018-10-28 20:39:49 +0300

    test:- * create a test for a function recursion*

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 const assert = require('assert');
 const sumOfOther = require('../src/sumOfOther');
 const make = require('../src/make.js');
+const recursion = require('../src/recursion.js');
+
 
 describe('sumOfOther', () => {
 	it('1', () => {
@@ -76,4 +78,37 @@ describe('make', () => {
     const result = make(2, 5, 100)(0)(sum);
     assert.equal(result, 1);
   });
+});
+
+describe('recursion', () => {
+	let tree = {
+			"value": 100,
+			"left": {
+					"value": 90,
+					"left": {
+							"value": 70
+					},
+					"right": {
+							"value": 99
+					}
+			},
+			"right": {
+					"value": 120,
+					"left": {
+							"value": 110
+					},
+					"right": {
+							"value": 130
+					}
+			}
+	};
+
+	it('should pass', () => {
+			assert.deepEqual(recursion(tree), [
+					[100],
+					[90, 120],
+					[70, 99, 110, 130]
+			])
+	});
+
 });
